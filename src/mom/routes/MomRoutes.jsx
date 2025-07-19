479affeea45207b442751d155a611179dc005ceb
GimhaniImasha <imashagimhani381@gmail.com>
2025-07-20 03:45:39 +0530

    appointments page created

--- src/mom/routes/MomRoutes.jsx
+++ src/mom/routes/MomRoutes.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import MomLayout from '../MomLayout';
 import Home from '../pages/Home';
-import Appointments from '../pages/Appointments';
 import Profile from '../pages/Profile';
 import BabyCostsCalculator from '../pages/BabyCostsCalculator';
 import BabyNameFinder from '../pages/PregnancyTrackerBabyNameFinder';
+import MyAppointments from '../pages/Appointments/MyAppointments';
 
 export default (
   <Route path="/mom" element={<MomLayout />}>
     <Route index element={<Home />} />
     <Route path="baby-costs-calculator" element={<BabyCostsCalculator />} />
-    <Route path="appointments" element={<Appointments />} />
     <Route path="profile" element={<Profile />} />
     <Route path="babynamefinder" element={<BabyNameFinder />} />
+    <Route path="myappointments" element={<MyAppointments />} />
     {/* Add more routes as needed */}
   </Route>
    
