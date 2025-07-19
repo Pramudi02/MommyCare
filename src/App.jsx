f38948fbb21466ba43798ff0abc9a3af1392eb7c
JithmiRandula <jithmirandula@gmail.com>
2025-07-20 01:24:55 +0530

    add auth modals: SignUp, navbar triggers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminRoutes from './admin/routes/AdminRoutes';
 import MomRoutes from './mom/routes/MomRoutes';
@@ -9,14 +9,39 @@ import MainNavbar from './components/MainNavbar/MainNavbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import HomeSection from './components/HomeSection/HomeSection';
 import MomNavbar from './mom/components/MomNavbar';
+import SignUp from './components/Authentication/SignUp/SignUp';
+import Login from './components/Authentication/Login/Login';
 
 function App() {
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleSignUpOpen = () => {
+    setShowSignUp(true);
+    setShowLogin(false);
+  };
+
+  const handleLoginOpen = () => {
+    setShowLogin(true);
+    setShowSignUp(false);
+  };
+
+  const handleCloseModals = () => {
+    setShowSignUp(false);
+    setShowLogin(false);
+  };
+
   return (
     <Router>
-      <MomNavbar/>
-      <MainNavbar />
+    
+      <MomNavbar
+        onSignUpClick={handleSignUpOpen}
+        onLoginClick={handleLoginOpen}
+      />
+
+      <MainNavbar/>
       <HeroSection />
-      <HomeSection/>
+      <HomeSection />
       <Routes>
         {AdminRoutes}
         {MomRoutes}
@@ -24,6 +49,10 @@ function App() {
         {DoctorRoutes}
         {SPRoutes}
       </Routes>
+      
+      {/* Modals */}
+      <SignUp isOpen={showSignUp} onClose={handleCloseModals} />
+      <Login isOpen={showLogin} onClose={handleCloseModals} />
     </Router>
   );
 }
