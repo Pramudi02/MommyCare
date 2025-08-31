9cf97fde758c6515e96e1d711c3fe639895a9e24
JithmiRandula <jithmirandula@gmail.com>
2025-08-31 15:51:29 +0530

    Add MidwifeFloatingChatWidget to MidwifeLayout and update routing to include MidwifeChat page

--- src/midwife/MidwifeLayout.jsx
+++ src/midwife/MidwifeLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import MidwifeNavbar from './components/MidwifeNavbar';
+import MidwifeFloatingChatWidget from '../components/MidwifeFloatingChatWidget';
 
 const MidwifeLayout = () => (
   <div>
     <MidwifeNavbar />
     <Outlet />
+    <MidwifeFloatingChatWidget />
   </div>
 );
 
